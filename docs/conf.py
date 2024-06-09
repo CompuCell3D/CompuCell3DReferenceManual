c87d5fc9734f82b42e7d9f9ddc885e0fec0df882
maciekswat <maciekswat@gmail.com>
2024-06-09 13:07:15 -0600

    added copy button to reference manual

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
+    'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
